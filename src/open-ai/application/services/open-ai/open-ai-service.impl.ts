a2f07d58d362b3819c04b4176b597d36831acf44
Pierre Viara <pierre.viara@outlook.com>
2023-12-13 20:53:09 +0100

    feat(openai): makes use of gpt-4

--- src/open-ai/application/services/open-ai/open-ai-service.impl.ts
+++ src/open-ai/application/services/open-ai/open-ai-service.impl.ts
@@ -38,7 +38,7 @@ export class OpenAIServiceImpl implements OpenAIService {
 
         try {
             const response = await openAIObject.chat.completions.create({
-                model: 'gpt-3.5-turbo-1106',
+                model: 'gpt-4-1106-preview',
                 response_format: { type: 'json_object' },
                 messages: [
                     {
