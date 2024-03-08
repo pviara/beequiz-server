b3d42fd859e1dc6e7a7a20bee217f83f4af2dc8e
Pierre Viara <pierre.viara@outlook.com>
2024-03-08 18:58:01 +0100

    tech(openai): edit provider to make it customizable (#14)

--- src/open-ai/application/services/open-ai/open-ai-service.provider.ts
+++ src/open-ai/application/services/open-ai/open-ai-service.provider.ts
@@ -1,9 +1,33 @@
+import { AppConfigService } from '../../../../infrastructure/app-config/app-config-service';
+import { APP_CONFIG_SERVICE_TOKEN } from '../../../../infrastructure/app-config/app-config-service.provider';
+import { OpenAIObjectFactory } from '../../open-ai-object-factory/open-ai-object-factory';
 import { OpenAIServiceImpl } from './open-ai-service.impl';
+import { OPENAI_OBJECT_FACTORY_TOKEN } from '../../open-ai-object-factory/open-ai-object-factory.provider';
+import { PromptService } from '../prompt/prompt-service';
 import { Provider } from '@nestjs/common';
+import { PROMPT_SERVICE_TOKEN } from '../prompt/prompt-service.provider';
+import { QuizParser } from '../../../../quiz/application/quiz-parser/quiz-parser';
+import { QUIZ_PARSER_TOKEN } from '../../../../quiz/application/quiz-parser/quiz-parser.provider';
 
 export const OPENAI_SERVICE_TOKEN = 'OpenAIService';
 
 export const OpenAIServiceProvider: Provider = {
     provide: OPENAI_SERVICE_TOKEN,
-    useClass: OpenAIServiceImpl,
+    inject: [
+        APP_CONFIG_SERVICE_TOKEN,
+        OPENAI_OBJECT_FACTORY_TOKEN,
+        PROMPT_SERVICE_TOKEN,
+        QUIZ_PARSER_TOKEN,
+    ],
+    useFactory: (
+        appConfigService: AppConfigService,
+        objectFactory: OpenAIObjectFactory,
+        promptService: PromptService,
+        quizParser: QuizParser,
+    ) => {
+        if (appConfigService.isAppInDevMode()) {
+            // todo: return new FakeOpenAIServiceImpl(); 
+        }
+        return new OpenAIServiceImpl(objectFactory, promptService, quizParser);
+    },
 };
