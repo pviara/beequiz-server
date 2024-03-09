7c79536b734a0d7e24837a66315ecc5dad29cdec
Pierre Viara <pierre.viara@outlook.com>
2024-03-09 16:37:41 +0100

    tech(openai): add fake generated questions (#14)

--- src/open-ai/application/services/open-ai/fake-open-ai-service.impl.ts
+++ src/open-ai/application/services/open-ai/fake-open-ai-service.impl.ts
@@ -1,16 +1,110 @@
 import { OpenAIService } from './open-ai-service';
-import { ParsedQuizQuestion } from '../../../../quiz/application/quiz-parser/model/parsed-quiz-question';
+import {
+    ParsedQuizAnswer,
+    ParsedQuizQuestion,
+} from '../../../../quiz/application/quiz-parser/model/parsed-quiz-question';
 import { ParsedQuizTheme } from '../../../../quiz/application/quiz-parser/model/parsed-quiz-theme';
 import { QuizQuestion } from '../../../../quiz/domain/quiz-question';
 import { QuizTheme } from '../../../../quiz/domain/quiz-parameters';
 
 export class FakeOpenAIServiceImpl implements OpenAIService {
-    generateQuestionsForQuiz(
+    async generateQuestionsForQuiz(
         existingQuestions: QuizQuestion[],
         numberOfQuestions: number,
         themeLabel: string,
     ): Promise<ParsedQuizQuestion[]> {
-        throw new Error('Method not implemented.');
+        return [
+            new ParsedQuizQuestion('questionA', [
+                new ParsedQuizAnswer('reponse A', false),
+                new ParsedQuizAnswer('reponse B', false),
+                new ParsedQuizAnswer('reponse C', true),
+                new ParsedQuizAnswer('reponse D', false),
+            ]),
+            new ParsedQuizQuestion('questionB', [
+                new ParsedQuizAnswer('reponse A', false),
+                new ParsedQuizAnswer('reponse B', false),
+                new ParsedQuizAnswer('reponse C', true),
+                new ParsedQuizAnswer('reponse D', false),
+            ]),
+            new ParsedQuizQuestion('questionC', [
+                new ParsedQuizAnswer('reponse A', false),
+                new ParsedQuizAnswer('reponse B', false),
+                new ParsedQuizAnswer('reponse C', true),
+                new ParsedQuizAnswer('reponse D', false),
+            ]),
+            new ParsedQuizQuestion('questionD', [
+                new ParsedQuizAnswer('reponse A', false),
+                new ParsedQuizAnswer('reponse B', false),
+                new ParsedQuizAnswer('reponse C', true),
+                new ParsedQuizAnswer('reponse D', false),
+            ]),
+            new ParsedQuizQuestion('questionE', [
+                new ParsedQuizAnswer('reponse A', false),
+                new ParsedQuizAnswer('reponse B', false),
+                new ParsedQuizAnswer('reponse C', true),
+                new ParsedQuizAnswer('reponse D', false),
+            ]),
+            new ParsedQuizQuestion('questionA', [
+                new ParsedQuizAnswer('reponse A', false),
+                new ParsedQuizAnswer('reponse B', false),
+                new ParsedQuizAnswer('reponse C', true),
+                new ParsedQuizAnswer('reponse D', false),
+            ]),
+            new ParsedQuizQuestion('questionB', [
+                new ParsedQuizAnswer('reponse A', false),
+                new ParsedQuizAnswer('reponse B', false),
+                new ParsedQuizAnswer('reponse C', true),
+                new ParsedQuizAnswer('reponse D', false),
+            ]),
+            new ParsedQuizQuestion('questionC', [
+                new ParsedQuizAnswer('reponse A', false),
+                new ParsedQuizAnswer('reponse B', false),
+                new ParsedQuizAnswer('reponse C', true),
+                new ParsedQuizAnswer('reponse D', false),
+            ]),
+            new ParsedQuizQuestion('questionD', [
+                new ParsedQuizAnswer('reponse A', false),
+                new ParsedQuizAnswer('reponse B', false),
+                new ParsedQuizAnswer('reponse C', true),
+                new ParsedQuizAnswer('reponse D', false),
+            ]),
+            new ParsedQuizQuestion('questionE', [
+                new ParsedQuizAnswer('reponse A', false),
+                new ParsedQuizAnswer('reponse B', false),
+                new ParsedQuizAnswer('reponse C', true),
+                new ParsedQuizAnswer('reponse D', false),
+            ]),
+            new ParsedQuizQuestion('questionA', [
+                new ParsedQuizAnswer('reponse A', false),
+                new ParsedQuizAnswer('reponse B', false),
+                new ParsedQuizAnswer('reponse C', true),
+                new ParsedQuizAnswer('reponse D', false),
+            ]),
+            new ParsedQuizQuestion('questionB', [
+                new ParsedQuizAnswer('reponse A', false),
+                new ParsedQuizAnswer('reponse B', false),
+                new ParsedQuizAnswer('reponse C', true),
+                new ParsedQuizAnswer('reponse D', false),
+            ]),
+            new ParsedQuizQuestion('questionC', [
+                new ParsedQuizAnswer('reponse A', false),
+                new ParsedQuizAnswer('reponse B', false),
+                new ParsedQuizAnswer('reponse C', true),
+                new ParsedQuizAnswer('reponse D', false),
+            ]),
+            new ParsedQuizQuestion('questionD', [
+                new ParsedQuizAnswer('reponse A', false),
+                new ParsedQuizAnswer('reponse B', false),
+                new ParsedQuizAnswer('reponse C', true),
+                new ParsedQuizAnswer('reponse D', false),
+            ]),
+            new ParsedQuizQuestion('questionE', [
+                new ParsedQuizAnswer('reponse A', false),
+                new ParsedQuizAnswer('reponse B', false),
+                new ParsedQuizAnswer('reponse C', true),
+                new ParsedQuizAnswer('reponse D', false),
+            ]),
+        ].slice(0, numberOfQuestions);
     }
     generateThemesForQuiz(
         existingThemes: QuizTheme[],
